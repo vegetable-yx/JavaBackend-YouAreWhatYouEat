547c7994d8662741a8746594ae85ec600662563b
Nikooooooooooo <82916991+NikoYoungdddd@users.noreply.github.com>
2022-12-03 17:09:08 +0800

    add dishes

---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/dishes/src/main/java/com/example/dishes/DishesApplication.java
@@ -0,0 +1,13 @@
+package com.example.dishes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishesApplication.class, args);
+    }
+
+}
---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/dishes/src/test/java/com/example/dishes/DishesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dishes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
