1c3c99c7829bd14b95351225b698622cd0a0d8d0
Nikooooooooooo <82916991+NikoYoungdddd@users.noreply.github.com>
2022-12-03 17:55:11 +0800

    搭好文件架构

--- YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/dishes/src/main/java/com/example/dishes/DishesApplication.java
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/dishes/src/main/java/com/example/dishes/DishesApplication.java
@@ -2,6 +2,7 @@ package com.example.dishes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
