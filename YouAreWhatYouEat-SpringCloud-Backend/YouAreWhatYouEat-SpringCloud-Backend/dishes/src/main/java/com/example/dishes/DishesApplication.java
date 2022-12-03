c8b249e56209188329a5c31e776a7257f0ff5226
Nikooooooooooo <82916991+NikoYoungdddd@users.noreply.github.com>
2022-12-03 17:28:30 +0800

    1

--- YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/dishes/src/main/java/com/example/dishes/DishesApplication.java
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/dishes/src/main/java/com/example/dishes/DishesApplication.java
@@ -3,6 +3,7 @@ package com.example.dishes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DishesApplication {
 
