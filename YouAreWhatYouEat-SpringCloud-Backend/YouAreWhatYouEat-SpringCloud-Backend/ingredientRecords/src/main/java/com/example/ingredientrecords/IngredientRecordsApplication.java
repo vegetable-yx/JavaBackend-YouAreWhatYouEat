3bf85a9a27a9d1dae273bb579bed91870556bfa9
Nikooooooooooo <82916991+NikoYoungdddd@users.noreply.github.com>
2022-12-03 17:26:20 +0800

    1

---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/ingredientRecords/src/main/java/com/example/ingredientrecords/IngredientRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingredientrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientRecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientRecordsApplication.class, args);
+    }
+
+}
---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/orderLists/src/main/java/com/example/orderlists/OrderListsApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderlists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderListsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderListsApplication.class, args);
+    }
+
+}
