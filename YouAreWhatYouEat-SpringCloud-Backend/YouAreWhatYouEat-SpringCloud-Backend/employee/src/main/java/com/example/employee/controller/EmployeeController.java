1adc897bcde6c4824a957e885ee99174f4ca2719
Xin Yang <yangxin092333@gmail.com>
2022-12-05 20:34:35 +0800

    基本完成employee controller

--- YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/employee/src/main/java/com/example/employee/controller/EmployeeController.java
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/employee/src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,16 +1,17 @@
 package com.example.employee.controller;
 
-import com.example.employee.EmployeeApplication;
-import com.example.employee.dto.AllEmployeeInfoDto;
-import com.example.employee.dto.OneEmployeeDto;
-import com.example.employee.entity.EmployeeEntity;
-import com.example.employee.entity.PrizeEntity;
+import com.example.employee.dto.AllEmployeeInfoOutDto;
+import com.example.employee.dto.EmployeeSimpleInfoOutDto;
+import com.example.employee.dto.OneEmployeeInDto;
+import com.example.employee.dto.OneEmployeeOutDto;
 import com.example.employee.service.EmployeeService;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -27,20 +28,46 @@ public class EmployeeController {
 
     // Get all employee info
     @RequestMapping(value = "employees", method = RequestMethod.GET)
-    public ResponseEntity<List<AllEmployeeInfoDto>> getAllEmployees(
+    public ResponseEntity<List<AllEmployeeInfoOutDto>> getAllEmployees(
             @RequestParam(value = "token", required = false) String token
     )
     {
         // check token
         // ......
 
-        List<AllEmployeeInfoDto> result = employeeService.getAllEmployeeInfo();
-        return new ResponseEntity<List<AllEmployeeInfoDto>>(result, HttpStatus.OK);
+        List<AllEmployeeInfoOutDto> result = employeeService.getAllEmployeeInfo();
+        if (result.size() == 0) return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<List<AllEmployeeInfoOutDto>>(result, HttpStatus.OK);
+    }
+
+    // Get all employee simple info
+    @RequestMapping(value = "employeeInfo", method = RequestMethod.GET)
+    public ResponseEntity<List<EmployeeSimpleInfoOutDto>> getAllEmployeeSimpleInfo()
+    {
+        List<EmployeeSimpleInfoOutDto> result = employeeService.getAllEmployeeSimpleInfo();
+        if (result.size() == 0) return new ResponseEntity(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    // Get one employee info
+    @RequestMapping(value = "employee", method = RequestMethod.GET)
+    public ResponseEntity<OneEmployeeOutDto> getAllEmployees(
+            @RequestParam(value = "id", required = true) String id,
+            @RequestParam(value = "token", required = false) String token
+    )
+    {
+        // check token
+        // ......
+        OneEmployeeOutDto result = employeeService.getOneEmployeeInfo(id);
+        if (result == null) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     // add a new employee
     @RequestMapping(value = "employees", method = RequestMethod.POST)
-    public ResponseEntity addEmployee(@ModelAttribute OneEmployeeDto employeeDto)
+    public ResponseEntity addEmployee(@ModelAttribute OneEmployeeInDto employeeDto)
     {
         if (employeeService.addEmployee(employeeDto)) {
             return new ResponseEntity(HttpStatus.CREATED);
@@ -51,7 +78,7 @@ public class EmployeeController {
 
     // update a new employee
     @RequestMapping(value = "employees", method = RequestMethod.PUT)
-    public ResponseEntity updateEmployee(@ModelAttribute OneEmployeeDto employeeDto)
+    public ResponseEntity updateEmployee(@ModelAttribute OneEmployeeInDto employeeDto)
     {
         if (employeeService.updateEmployee(employeeDto)) {
             return new ResponseEntity(HttpStatus.OK);
---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/employee/src/main/java/com/example/employee/dto/PayrollInfo.java
@@ -0,0 +1,9 @@
+package com.example.employee.dto;
+
+import lombok.Data;
+
+@Data
+public class PayrollInfo {
+    private String pay_datetime;
+    private Double amount;
+}
