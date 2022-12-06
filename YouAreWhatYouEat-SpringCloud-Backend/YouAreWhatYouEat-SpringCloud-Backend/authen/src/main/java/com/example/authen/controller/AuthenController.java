b81214ea8efca4e791c97ae61d3a1910a1cb61e5
Xin Yang <yangxin092333@gmail.com>
2022-12-06 19:12:28 +0800

    完成authen controller，以及employee token鉴权

---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/authen/src/main/java/com/example/authen/controller/AuthenController.java
@@ -0,0 +1,34 @@
+package com.example.authen.controller;
+
+import com.example.authen.entity.MyToken;
+import com.example.authen.service.MyTokenService;
+import jakarta.annotation.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("api/v1/")
+public class AuthenController {
+    @Resource
+    private MyTokenService myTokenService;
+
+    @RequestMapping(value = "tokenLegality", method = RequestMethod.GET)
+    public ResponseEntity<Boolean> checkToken(
+            @RequestParam(required = false) String token
+    ) throws IOException {
+        System.out.println("Hi");
+        try {
+            MyToken myToken = myTokenService.checkToken(token);
+            if (myToken.isActive()) return new ResponseEntity<>(true, HttpStatus.OK);
+            else return new ResponseEntity<>(false, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+}
---
+++ YouAreWhatYouEat-SpringCloud-Backend/YouAreWhatYouEat-SpringCloud-Backend/employee/src/main/java/com/example/employee/service/AuthenClient.java
@@ -0,0 +1,15 @@
+package com.example.employee.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "authen", url = "localhost:18084/api/v1")
+public interface AuthenClient {
+    @RequestMapping(value = "tokenLegality", method = RequestMethod.GET)
+    public ResponseEntity<Boolean> checkToken(
+            @RequestParam(required = false) String token
+    );
+}
